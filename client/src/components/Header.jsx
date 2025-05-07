c9da15a3cd9eda40270a40fce0a7d7481982a14c
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-05-07 23:50:38 +0530

    Updated Fronted

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,10 +11,29 @@ const Header = () => {
         Turn text to <span className="text-blue-600">image</span>, in seconds.
       </h1>
 
-      <p className='text-center max-w-xl mx-auto mt-5'>
+      <p className="text-center max-w-xl mx-auto mt-5">
         Unleash your creativity with AI. Turn your imagination into visual art
         in seconds - just type, and watch the magic happen.
       </p>
+      <button className="sm:text-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full">
+        Generate Images <img className="h-6" src={assets.star_group} alt="" />
+      </button>
+
+      <div className="flex flex-wrap justify-center mt-16 gap-3">
+        {Array(6)
+          .fill("")
+          .map((items, index) => (
+            <img
+              className="rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10"
+              src={ index % 2 === 0 ? assets.sample_img_2 : assets.sample_img_1}
+              alt=""
+              key={index}
+              width={70}
+            />
+          ))}
+      </div>
+<p className="mt-2 text-neutral-600">Generated images form Imegify</p>
+
     </div>
   );
 };
