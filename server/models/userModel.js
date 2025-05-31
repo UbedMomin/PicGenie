81389427a4f66a36937c4958cc74eb53bed6c5f3
ubedmomin2017@gmail.com <ubedmomin2017@gmail.com>
2025-05-31 23:29:10 +0530

    ServerSchemas

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,12 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    
-})
+  name: { Type: String, required: true },
+  email: { Type: String, required: true, unique: true },
+  password: { Type: String, required: true },
+  creditBalance: {type: Number, default:5 },
+});
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema)
+
+export default userModel;
